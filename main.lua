f67369dda4ff377801657748848cb1716f145f08
Magnus Backhurst <datamange@gmail.com>
2018-11-12 11:16:45 +0100

    Initial base structure of the game

---
+++ main.lua
@@ -0,0 +1,14 @@
+function love.load()
+end
+
+function love.update(dt)
+end
+
+function love.keypressed(key)
+    if key == 'escape' then
+        love.event.push('quit')
+    end
+end
+
+function love.draw(dt)
+end
